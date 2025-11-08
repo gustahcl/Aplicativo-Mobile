e990639ab0d493f0321c5d0a9221ddbe91fa977a
Gustavo Henrique <guustavo.henrique.c.l12@gmail.com>
2025-11-07 23:21:38 -0300

    curriculo

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Esta linha é a mais importante:
     plugins: ['nativewind/babel'],
   };
 };
